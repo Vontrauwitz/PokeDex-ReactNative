e373caf2081b5428d83526f3cd17bf958bd85cd8
Hans F. Trauwitz V <hans.trauwitz@gmail.com>
2022-12-12 02:15:34 -0600

    favoritos se actualizan con useFocusEffect y con useCallBack

--- src/screens/Favorites.js
+++ src/screens/Favorites.js
@@ -1,22 +1,49 @@
-import React from 'react'
-import { Text, Button } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import React, { useState, useEffect, useCallback } from 'react'
+import { Text } from 'react-native'
+import { useFocusEffect } from '@react-navigation/core'
 import { getPokemonsFavoriteApi } from '../api/favorite'
+import { getPokemonDetailsApi } from '../api/Pokemon'
+import useAuth from '../hooks/useAuth'
+import PokemonList from './../components/PokemonList';
 
 
 export default function Favorites() {
+  const [pokemons, setPokemons] = useState([]);
+  const { auth } = useAuth();
 
-  const checkFavorites = async () => {
-    const response = await getPokemonsFavoriteApi();
-    console.log(response);
-  }
+  useFocusEffect(
+    useCallback(() => {
+      if (auth) {
+        (async () => {
+          const response = await getPokemonsFavoriteApi();
 
+          const pokemonsArray = [];
+          for await (const id of response) {
 
+            const pokemonDetails = await getPokemonDetailsApi(id);
+            pokemonsArray.push({
+              id: pokemonDetails.id,
+              name: pokemonDetails.name,
+              type: pokemonDetails.types[0].type.name,
+              order: pokemonDetails.order,
+              image: pokemonDetails.sprites.other["official-artwork"].front_default,
+            });
+          }
+          setPokemons(pokemonsArray)
+        })()
+      }
 
-  return (
-    <SafeAreaView>
-      <Text>Favorites</Text>
-      <Button title='Obtener Favoritos' onPress={checkFavorites} />
-    </SafeAreaView>
+    }, [auth])
   )
+
+
+
+
+
+
+  return !auth ? (
+    <Text>User not Logged</Text>
+  ) : (
+    <PokemonList pokemons={pokemons} />
+  );
 }
